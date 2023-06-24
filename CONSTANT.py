3fe89164271a4c510841208e6c50ebb19815faae
trinhminh11 <72061760+trinhminh11@users.noreply.github.com>
2023-06-24 15:05:53 +0700

    Update CONSTANT.py

--- CONSTANT.py
+++ CONSTANT.py
@@ -1,10 +1,9 @@
 data_num = 20     # Run how many time per 1 Dataset
 dataset_num = 6  # Dataset
-H = 2000		  # Region
+H = 2000	  # Region
 
 n_step = 150		  # step of n
 Rs_step = 15		  # step of Rs
-Qmax_step = 5	  # step of Qmax
+Qmax_step = 2	  # step of Qmax
 
 random_seed = 1
-
